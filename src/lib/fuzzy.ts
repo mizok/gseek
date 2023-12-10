014562f7c933ff745b61933f44749262c03301f0
mizok <karasunohina@gmail.com>
2023-12-11 01:50:17 +0800

    fix: fix powershell issue

--- src/lib/fuzzy.ts
+++ src/lib/fuzzy.ts
@@ -8,11 +8,13 @@ export function fuzzySearchBranches(key: string) {
   try {
     const platform = os.platform()
     let command = `git branch -a | Select-String -Pattern "${key}"`
-    if (platform === 'linux' || platform === 'darwin') {
+    const isLinuxLike = platform === 'linux' || platform === 'darwin';
+    if (isLinuxLike) {
       command = `git branch -a | grep '${key}'`
     }
     const results = execSync(command, {
       encoding: 'utf-8',
+      shell: isLinuxLike ? undefined : 'powershell.exe'
     })
       .split('\n')
       .map((x) => x.trim())
